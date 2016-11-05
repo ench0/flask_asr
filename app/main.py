d319ecca8a13b072c72854c87fc5a16be0d28ebd
ench0 <ensar@farend.net>
2016-11-05 14:56:10 +0000

    wsgi

--- app/main.py
+++ app/main.py
@@ -4,6 +4,6 @@ import views
 
 if __name__ == '__main__':
 #	app.run()
-	app.run(host='0.0.0.0', port=8080) # accessible from outside
+	app.run(host='0.0.0.0', port=5000) # accessible from outside
 # We do not call app.run(debug=True) because we have already instructed Flask to
 # run our app in the debug mode in the Configuration object.
