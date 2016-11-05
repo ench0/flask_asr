cec3937d0efed7dc92706bb61e303f1ae8b63b14
ICCI Admin <admin@ISLAM.LAN>
2016-11-05 17:49:02 +0000

    template

--- app/views.py
+++ app/views.py
@@ -3,9 +3,8 @@ from app import app
 
 @app.route('/')
 def homepage():
-	# name = request.args.get('name')
-	# number = request.args.get('number')
-	# if not name:
-		# name = '<unknown>' #http://127.0.0.1:5000/?name=Charlie
-	# return render_template('homepage.html', name=name, number=number)
-	return render_template('homepage.html')
+	name = request.args.get('name')
+	number = request.args.get('number')
+	if not name:
+		name = '<unknown>' #http://127.0.0.1:5000/?name=Charlie
+	return render_template('homepage.html', name=name, number=number)
