31326f6e4e91983a9cb283a09d098feae2346540
ICCI Admin <admin@ISLAM.LAN>
2016-11-07 14:05:31 +0000

    identation fix

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -97,15 +97,15 @@ def delete(slug):
 
 # images
 @posts.route('/image-upload/', methods=['GET', 'POST'])
-    def image_upload():
-        if request.method == 'POST':
-            form = ImageForm(request.form)
-            if form.validate():
-                image_file = request.files['file']
-                filename = os.path.join(app.config['IMAGES_DIR'], secure_filename(image_file.filename))
-                image_file.save(filename)
-                flash('Saved %s' % os.path.basename(filename), 'success')
-                return redirect(url_for('posts.index'))
-        else:
-            form = ImageForm()
-        return render_template('posts/image_upload.html', form=form)
+def image_upload():
+    if request.method == 'POST':
+        form = ImageForm(request.form)
+        if form.validate():
+            image_file = request.files['file']
+            filename = os.path.join(app.config['IMAGES_DIR'], secure_filename(image_file.filename))
+            image_file.save(filename)
+            flash('Saved %s' % os.path.basename(filename), 'success')
+            return redirect(url_for('posts.index'))
+    else:
+        form = ImageForm()
+    return render_template('posts/image_upload.html', form=form)
