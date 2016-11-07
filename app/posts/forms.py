d95f06fa4b1f34ab1e0c0755183e04969e64015d
ICCI Admin <admin@ISLAM.LAN>
2016-11-07 11:03:00 +0000

    form validation

--- app/posts/forms.py
+++ app/posts/forms.py
@@ -1,16 +1,23 @@
 import wtforms
+from wtforms.validators import DataRequired
 
 from models import Post
 
+
 class PostForm(wtforms.Form):
-    title = wtforms.StringField('Title')
-    body = wtforms.TextAreaField('Body')
+    title = wtforms.StringField(
+        'Title',
+        validators=[DataRequired()])
+    body = wtforms.TextAreaField(
+        'Body',
+        validators=[DataRequired()])
     status = wtforms.SelectField(
         'Post status',
         choices=(
             (Post.STATUS_PUBLIC, 'Public'),
             (Post.STATUS_DRAFT, 'Draft')),
         coerce=int)
+
     def save_post(self, post):
         self.populate_obj(post)
         post.generate_slug()
