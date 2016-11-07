94a4dda4ec7dac5eb11c7b0d6f5650b0e45464f1
Ensar <ensar@localhost.localdomain>
2016-11-07 20:08:57 +0000

    str

--- app/models.py
+++ app/models.py
@@ -76,7 +76,7 @@ class User(db.Model):
 
 	# Flask-Login interface..
 	def get_id(self):
-		return int(self.id, 'utf-8')
+		return str(self.id) #return str(self.id, 'utf-8')
 	def is_authenticated(self):
 		return True
 	def is_active(self):
