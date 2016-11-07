33bdbed6add5cf0abb1197fee76c9f9fc982b532
ICCI Admin <admin@ISLAM.LAN>
2016-11-07 15:41:34 +0000

    login form

--- app/views.py
+++ app/views.py
@@ -1,10 +1,22 @@
-from flask import render_template, request
-from app import app
+from flask import flash, redirect, render_template, request, url_for
+from flask_login import login_user
 
-@app.route('/')
+from app import app
+from app import login_manager
+from forms import LoginForm
+
+@app.route("/")
 def homepage():
-	name = request.args.get('name')
-	number = request.args.get('number')
-	if not name:
-		name = '<unknown>' #http://127.0.0.1:5000/?name=Charlie
-	return render_template('homepage.html', name=name, number=number)
+	return render_template("homepage.html")
+
+@app.route("/login/", methods=["GET", "POST"])
+def login():
+	if request.method == "POST":
+		form = LoginForm(request.form)
+		if form.validate():
+			login_user(form.user, remember=form.remember_me.data)
+			flash("Successfully logged in as %s." % form.user.email, "success")
+			return redirect(request.args.get("next") or url_for("homepage"))
+	else:
+		form = LoginForm()
+	return render_template("login.html", form=form)
