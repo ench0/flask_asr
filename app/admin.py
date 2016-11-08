0e4e8b792e39445cababdfade7e8ec832e53bd72
FarendNet Makina <farend@FarendNets-Mac-Pro.local>
2016-11-08 13:42:59 +0000

    admin2

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,12 @@
 from flask_admin import Admin
 #from flask.ext.admin import Admin #depreciated
 # pip install Flask-Admin
-from app import app
+from flask_admin.contrib.sqla import ModelView
+
+from app import app, db
+from models import Post, Tag, User
 
 admin = Admin(app, 'Blog Admin')
+admin.add_view(ModelView(Post, db.session))
+admin.add_view(ModelView(Tag, db.session))
+admin.add_view(ModelView(User, db.session))
