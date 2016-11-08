944e4eb78e0ebd273fbfe092e230f9fad37335e9
FarendNet Makina <farend@FarendNets-Mac-Pro.local>
2016-11-08 14:59:25 +0000

    slug update

--- app/admin.py
+++ app/admin.py
@@ -4,11 +4,22 @@ from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 
 from app import app, db
-from models import Post, Tag, User
+from models import Post, Tag, User, post_tags
 from wtforms.fields import SelectField # At top of module.
 from wtforms.fields import PasswordField # At top of module.
 
-class PostModelView(ModelView):
+# fixing slugs
+class BaseModelView(ModelView):
+    pass
+
+
+class SlugModelView(BaseModelView):
+    def on_model_change(self, form, model, is_created):
+        model.generate_slug()
+        return super(SlugModelView, self).on_model_change(form, model, is_created)
+
+
+class PostModelView(SlugModelView):
     _status_choices = [(choice, label) for choice, label in [
         (Post.STATUS_PUBLIC, 'Public'),
         (Post.STATUS_DRAFT, 'Draft'),
@@ -17,47 +28,39 @@ class PostModelView(ModelView):
     column_choices = {
         'status': _status_choices,
     }
-    column_filters = [
-    'status', User.name, User.email, 'created_timestamp'
-    ]
+    column_filters = ['status', User.name, User.email, 'created_timestamp']
     column_list = [
         'title', 'status', 'author', 'tease', 'tag_list',
         'created_timestamp',
     ]
     column_searchable_list = ['title', 'body']
-    column_select_related_list = ['author'] # Efficiently SELECT the author.
-    form_args = {
-        'status': {'choices': _status_choices, 'coerce': int},
-    }
-    form_columns = ['title', 'body', 'status', 'author', 'tags']
-    form_overrides = {'status': SelectField}
+    column_select_related_list = ['author']
     form_ajax_refs = {
         'author': {
             'fields': (User.name, User.email),
         },
     }
+    form_args = {
+        'status': {'choices': _status_choices, 'coerce': int},
+    }
+    form_columns = ['title', 'body', 'status', 'author', 'tags']
+    form_overrides = {'status': SelectField}
 
 
-class UserModelView(ModelView):
+class UserModelView(SlugModelView):
     column_filters = ('email', 'name', 'active')
     column_list = ['email', 'name', 'active', 'created_timestamp']
     column_searchable_list = ['email', 'name']
     form_columns = ['email', 'password', 'name', 'active']
-
     form_extra_fields = {
         'password': PasswordField('New password'),
-        }
+    }
     def on_model_change(self, form, model, is_created):
         if form.password.data:
             model.password_hash = User.make_password(form.password.data)
         return super(UserModelView, self).on_model_change(form, model, is_created)
 
-
-
 admin = Admin(app, 'Blog Admin')
 admin.add_view(PostModelView(Post, db.session))
-admin.add_view(ModelView(Tag, db.session))
-#admin.add_view(ModelView(User, db.session))
-
-# Be sure to use the UserModelView class when registering the User:
+admin.add_view(SlugModelView(Tag, db.session))
 admin.add_view(UserModelView(User, db.session))
